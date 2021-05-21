c689be3c6d490ddae65e388a896bef0464fb2456
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-05-21 14:38:45 +0700

    first commit

---
+++ app/src/main/java/com/example/lab9_18056521_hahuyhoang/Act1.java
@@ -0,0 +1,14 @@
+package com.example.lab9_18056521_hahuyhoang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act1);
+    }
+}
